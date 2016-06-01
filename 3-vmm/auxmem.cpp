044a80018b0ce93d763c480acbadd6a9f93926e8
libo <436101042@qq.com>
2016-06-01 13:13:23 +0000

    aux

--- 3-vmm/auxmem.cpp
+++ 3-vmm/auxmem.cpp
@@ -1,11 +1,26 @@
 #include "auxmem.h"
-
+#include "global.h"
 auxMem::auxMem() {
+    if ((memFile = fopen(AUX_FILE, O_RDWR)) < 0){
+        error_sys("Aux file open failed.");
+    }
 }
 
 void auxMem::writePage(unsigned char* content, unsigned int addr) {
+    if (fseek(memFile,addr,SEEK_SET)<0){
+        error_sys("fseek failed.");
+    }
+    if (fwrite(content, 1, PAGE_SIZE, memFile) < PAGE_SIZE){
+        error_sys("fwrite failed.");
+    };
 }
 
 void auxMem::readPage(unsigned char* content, unsigned int addr) {
+    if (fseek(memFile,addr,SEEK_SET)<0){
+        error_sys("fseek failed.");
+    }
+    if (fread(content, 1, PAGE_SIZE, memFile) < PAGE_SIZE){
+        error_sys("fread failed.");
+    };
 }
 
--- 3-vmm/global.h
+++ 3-vmm/global.h
@@ -5,5 +5,11 @@
 #define ACTMEM_SIZE (256*1048576)
 #define AUXMEM_SIZE (1024*1048576)
 
-#endif
+#define AUX_FILE "auxMem"
 
+void error_sys(const char* msg){
+    perror(msg);
+    exit(1);
+};
+
+#endif
