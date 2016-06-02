8e0bbc9484a204d8baed6623bd03bfe7cc931cf6
linziyi000 <linziyi@aliyun.com>
2016-06-02 12:53:13 +0800

    Update global.h

--- 3-vmm/global.h
+++ 3-vmm/global.h
@@ -12,4 +12,12 @@
 
 void error_sys(const char* msg);
 
+typedef unsigned int pte_t;
+
+inline int ptePresent(pte_t x) { return x&1; }
+inline int pteAllocated(pte_t x) { return (x>>1)&1; }
+inline int pteAllowRead(pte_t x) { return (x>>2)&1; }
+inline int pteAllowWrite(pte_t x) { return (x>>3)&1; }
+inline int pteAddr(pte_t x) { return (x>>12); }
+
 #endif
